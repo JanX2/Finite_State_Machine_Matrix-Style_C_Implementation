ee9a225c42ae8fea6dd8b3e1ab2c31a09616749e
Jan Weiß <jan@geheimwerk.de>
2017-03-30 21:23:23 +0200

    Initial Commit

---
+++ Finite_State_Machine_Matrix-Style_C_Implementation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Finite_State_Machine_Matrix-Style_C_Implementation
+//
+//  Created by Jan on 30/03/17.
+//  Copyright © 2017 Jan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
